7471b31cc41c806e0eed69ec73cf07f10e73d2d7
Michael Prather <mprather1@gmail.com>
2017-05-29 02:00:42 +0000

    uglify can't parse es6

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-export default class Config {
-  constructor (file) {
-    this.file = file
-  }
-  getConfig (key) {
-    var env
-    switch (window.location.hostname) {
-      case 'shintech.ninja':
-        env = 'production'
-        break
-      case 'dev.shintech.ninja':
-        env = 'development'
-        break
-      default:
-        throw (new Error('Unknown environment: ' + window.location.hostname))
-    }
-
-    return this.file[env][key]
-  }
+export default function Config (file) {
+  this.file = file
 }
+
+Config.prototype.getConfig = function (key) {
+  var env
+  switch (window.location.hostname) {
+    case 'shintech.ninja':
+      env = 'production'
+      break
+    case 'dev.shintech.ninja':
+      env = 'development'
+      break
+    default:
+      throw (new Error('Unknown environment: ' + window.location.hostname))
+  }
+
+  return this.file[env][key]
+}
+
+Config.prototype.constructor = Config
